1f9fdbf57198be64c04e97e591405fd9a982a998
Pablo Maldonado <pablo.maldonado@actum.cz>
2017-01-25 13:30:35 +0100

    dates can be int

--- zin_sklik.py
+++ zin_sklik.py
@@ -28,11 +28,19 @@ class Campaigns:
                
         end_date = parameters.get('end_date')
         
+        ## Trick for the dates
         if(type(end_date) == int):
             offset = end_date
             
             end_date = datetime.today() + timedelta(days=offset)
             end_date = end_date.strftime("%Y-%m-%d")
+
+        # start date            
+        if(type(start_date) == int):
+            offset = start_date
+            
+            start_date = datetime.today() + timedelta(days=offset)
+            start_date = end_date.strftime("%Y-%m-%d")
         
         
         s = xmlrpc.client.ServerProxy('https://api.sklik.cz/cipisek/RPC2')
